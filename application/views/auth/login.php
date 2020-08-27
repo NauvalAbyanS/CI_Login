bf274da867851f0a444cb7a455644d3d89c175c4
NauvalAbyanS <nabyan98.na@gmail.com>
2020-08-27 09:07:07 +0700

    initialize

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -3,18 +3,19 @@
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
-      <div class="col-xl-10 col-lg-10 col-md-9">
+      <div class="col-xl-9 col-lg-10 col-md-9">
 
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-body p-2">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-3"></div>
-              <div class="col-lg-6">
+              <div class="col-lg-2"></div>
+              <div class="col-lg-8">
                 <div class="p-5">
                   <div class="center">
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                    <?= $this->session->flashdata('message'); ?>
                   </div>
                   <form class="user">
                     <div class="form-group">
@@ -38,7 +39,7 @@
                     <a class="small" href="forgot-password.html">Forgot Password?</a>
                   </div>
                   <div class="text-center">
-                    <a class="small" href="register.html">Create an Account!</a>
+                    <a class="small" href="<?= base_url('auth/register'); ?>">Create an Account!</a>
                   </div>
                 </div>
               </div>
--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -1,45 +1,46 @@
   <div class="container">
 
-    <div class="card o-hidden border-0 shadow-lg my-5">
+    <div class="card o-hidden border-0 shadow-lg my-5 mx col-md-7 mx-auto">
       <div class="card-body p-0">
         <!-- Nested Row within Card Body -->
         <div class="row">
           <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
-          <div class="col-lg-7">
+          <div class="col-lg-12">
             <div class="p-5">
               <div class="text-center">
                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
               </div>
-              <form class="user">
+              <form class="user" method="post" action="<?= base_url('auth/register'); ?>">
                 <div class="form-group row">
-                  <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
-                  </div>
-                  <div class="col-sm-6">
-                    <input type="text" class="form-control form-control-user" id="exampleLastName" placeholder="Last Name">
+                  <div class="col-sm-12 mb-3 mb-sm-0">
+                    <input type="text" name="name" class="form-control form-control-user form-rounded" id="exampleFirstName" placeholder="Full Name">
+                  <?=form_error('name','<small class="text-danger pl-3">','</small>');?>
                   </div>
                 </div>
                 <div class="form-group">
-                  <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+                  <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+                  <?=form_error('email','<small class="text-danger pl-3">','</small>');?>
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                    <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                    <?=form_error('password','<small class="text-danger pl-3">','</small>');?>
                   </div>
                   <div class="col-sm-6">
-                    <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
+                    <input type="password" name="repeat_password"class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
+                    <?=form_error('repeat_password','<small class="text-danger pl-3">','</small>');?>
                   </div>
                 </div>
-                <a href="login.html" class="btn btn-primary btn-user btn-block">
+                <button type="submit" class="btn btn-primary btn-user btn-block">
                   Register Account
-                </a>
+                </button>
               </form>
               <hr>
               <div class="text-center">
                 <a class="small" href="forgot-password.html">Forgot Password?</a>
               </div>
               <div class="text-center">
-                <a class="small" href="login.html">Already have an account? Login!</a>
+                <a class="small" href="<?= base_url('auth'); ?>">Already have an account? Login!</a>
               </div>
             </div>
           </div>
